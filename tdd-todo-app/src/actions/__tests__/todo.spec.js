428eb87cee847bbd8f942d4a0abc92deaee75acd
Yuji Takaesu <taka.esu.user@gmail.com>
2017-07-22 14:39:17 +0900

    Impliments action tempolary

---
+++ tdd-todo-app/src/actions/__tests__/todo.spec.js
@@ -0,0 +1,32 @@
+import * as actions from '../todos'
+
+describe('todos action', () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'todo1',
+    },
+    // {
+    //   id: 2,
+    //   title: 'todo2',
+    // }
+  ]
+
+  describe('action creator', () => {
+    test('create FETCH_TODOS_SUCCESS', () => {
+      expect(actions.fetchTodosSuccess(todos)).toEqual({
+        type: 'FETCH_TODOS_SUCCESS',
+        todos
+      })
+    })
+  })
+
+  describe('fetchTodos', () => {
+    test('return action', () => {
+      expect(actions.fetchTodos()).toEqual({
+        type: 'FETCH_TODOS_SUCCESS',
+        todos,
+      })
+    })
+  })
+})
---
+++ tdd-todo-app/src/actions/todos.js
@@ -0,0 +1,21 @@
+export const fetchTodosSuccess = (todos) => {
+  return {
+    type: 'FETCH_TODOS_SUCCESS',
+    todos // キーとバリューが一緒であれば省略可能 「todos: todos」 と同じ
+  }
+}
+
+export const fetchTodos = () => {
+  const todos = [
+    {
+      id: 1,
+      title: 'todo1',
+    }
+  ]
+
+  return fetchTodosSuccess(todos)
+  // {
+  //   type: 'FETCH_TODO',
+  //   todos,   // キーとバリューが一緒であれば省略可能 「todos: todos」 と同じ
+  // }
+}
